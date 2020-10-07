0097b85eabf1ae1b6beb1666465cd09244580752
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-07 16:32:08 +0700

    Add files via upload

---
+++ tugas2_1.php
@@ -0,0 +1,16 @@
+<?php 
+
+$hari = 36;
+$pekerja = 12;
+
+$pekerja2 = 24; 
+
+$hari2 = ($hari * $pekerja) / $pekerja2;
+
+echo $hari, " Hari = ", $pekerja, " Pekerja <br>";
+echo "X hari = ", $pekerja2, " Pekerja <br>";
+echo "X  = ....? <br>";
+echo "Jawaban  :  ", $hari2;
+
+
+ ?>
